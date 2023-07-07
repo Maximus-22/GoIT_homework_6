ded800a2f74d9e969a6dd5330cb73039a8c246e1
Maximus-22 <maksim.melnichenko@nservice.com.ua>
2023-07-07 23:59:28 +0300

    Create normalize.py
    Function for translate done.

---
+++ normalize.py
@@ -0,0 +1,17 @@
+import re
+
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", \
+               "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "ts", "ch", \
+               "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
+TRANS = {}
+
+for cyr_symb, letter in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(cyr_symb)] = letter
+    TRANS[ord(cyr_symb.upper())] = letter.upper()
+
+
+def normalize(name: str) -> str:
+    transl_name = name.translate(TRANS)
+    transl_name = re.sub(r'\W', '_', transl_name)
+    return transl_name
