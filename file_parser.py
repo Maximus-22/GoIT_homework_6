8e01941c25f1aa1f44c0a088cbe20448eb33bbca
Maximus-22 <maksim.melnichenko@nservice.com.ua>
2023-07-08 18:13:39 +0300

    Create structure of parsing files
    Included all extensions which specified on the homework.

--- file_parser.py
+++ file_parser.py
@@ -6,15 +6,15 @@ JPG_IMAGES = []
 PNG_IMAGES = []
 SVG_IMAGES = []
 
-AVI_AUDIO = []
-MKV_AUDIO = []
-MOV_AUDIO = []
-MP3_AUDIO = []
-
-AMR_VIDEO = []
+AVI_VIDEO = []
+MKV_VIDEO = []
+MOV_VIDEO = []
 MP4_VIDEO = []
-OGG_VIDEO = []
-WAV_VIDEO = []
+
+AMR_AUDIO = []
+MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
 
 DOC_DOCUMENTS = []
 DOCX_DOCUMENTS = []
@@ -31,8 +31,8 @@ ZIP_ARCHIVES = []
 MY_OTHER = []
 
 REGISTER_EXTENSION = { "JPEG": JPEG_IMAGES, "JPG": JPG_IMAGES, "PNG": PNG_IMAGES, "SVG": SVG_IMAGES,
-                       "AVI": AVI_AUDIO, "MKV": MKV_AUDIO,"MOV": MOV_AUDIO, "MP4": MP3_AUDIO,
-                       "AMR": AMR_VIDEO, "MP3": MP4_VIDEO,"OGG": OGG_VIDEO, "WAV": WAV_VIDEO,
+                       "AVI": AVI_VIDEO, "MKV": MKV_VIDEO,"MOV": MOV_VIDEO, "MP4": MP4_VIDEO,
+                       "AMR": AMR_AUDIO, "MP3": MP3_AUDIO,"OGG": OGG_AUDIO, "WAV": WAV_AUDIO,
                        "DOC": DOC_DOCUMENTS, "DOCX": DOCX_DOCUMENTS,"PDF": PDF_DOCUMENTS, "PPTX": PPTX_DOCUMENTS,
                        "TXT": TXT_DOCUMENTS, "XLS": XLS_DOCUMENTS, "XLSX": XLSX_DOCUMENTS, 
                        "GZ": GZ_ARCHIVES, "TAR": TAR_ARCHIVES, "ZIP": ZIP_ARCHIVES}
@@ -49,7 +49,7 @@ def scan_and_transfer(folder: Path) -> None:
         # Якщо це папка то додаємо її до списку FOLDERS і переходимо до наступного елемента папки
         if item.is_dir():
             # перевіряємо, щоб папка не була тією в яку ми складаємо вже файли
-            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+            if item.name not in ("archives", "video", "audio", "documents", "images", "MY_OTHER"):
                 FOLDERS.append(item)
                 # скануємо вкладену папку
                 scan_and_transfer(item)  # рекурсія
@@ -73,16 +73,16 @@ def scan_and_transfer(folder: Path) -> None:
 
 if __name__ == "__main__":
     folder_to_scan = sys.argv[1]
-    print(f'Start in folder {folder_to_scan}')
+    print(f"Start in folder {folder_to_scan}")
     scan_and_transfer(Path(folder_to_scan))
-    # print(f'Images jpeg: {JPEG_IMAGES}')
-    # print(f'Images jpg: {JPG_IMAGES}')
-    # print(f'Images svg: {SVG_IMAGES}')
-    # print(f'Audio mp3: {MP3_AUDIO}')
-    # print(f'Zip archives: {ZIP_ARCHIVES}')
+    # print(f"Images jpeg: {JPEG_IMAGES}")
+    # print(f"Images jpg: {JPG_IMAGES}")
+    # print(f"Images svg: {SVG_IMAGES}")
+    # print(f"Audio mp3: {MP3_AUDIO}")
+    # print(f"Zip archives: {ZIP_ARCHIVES}")
 
-    print(f'Types of files in folder: {EXTENSION}')
-    print(f'Unknown files of types: {UNKNOWN}')
-    print(f'Not sorted: {MY_OTHER}')
+    print(f"Types of files in folder: {EXTENSION}")
+    print(f"Unknown files of types: {UNKNOWN}")
+    print(f"Not sorted: {MY_OTHER}")
 
     # print(FOLDERS)
--- main.py
+++ main.py
@@ -1,8 +1,96 @@
 from pathlib import Path
+import shutil
+import sys
+import file_parser as parser
+from normalize import normalize
 
 
-def main():
-    print("Have a nice day!")
+def handle_defined(filename: Path, target_folder: Path) -> None:
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+
+def handle_other(filename: Path, target_folder: Path) -> None:
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+
+def handle_archive(filename: Path, target_folder: Path) -> None:
+    target_folder.mkdir(exist_ok=True, parents=True)  # робимо папку для архіва
+    folder_for_file = target_folder / normalize(filename.name.replace(filename.suffix, ""))
+    folder_for_file.mkdir(exist_ok=True, parents=True)
+    try:
+        shutil.unpack_archive(filename, folder_for_file)  # TODO: Check!
+    except shutil.ReadError:
+        print(f"File {filename} is not archive")
+        folder_for_file.rmdir()
+    filename.unlink()
+
+
+def handle_folder(folder: Path):
+    try:
+        folder.rmdir()
+    except OSError:
+        print(f"Can\'t delete folder: {folder}")
+
+
+def main(folder: Path):
+    parser.scan_and_transfer(folder)
+    for file in parser.JPEG_IMAGES:
+        handle_defined(file, folder / "images" / "JPEG")
+    for file in parser.JPG_IMAGES:
+        handle_defined(file, folder / "images" / "JPG")
+    for file in parser.PNG_IMAGES:
+        handle_defined(file, folder / "images" / "PNG")
+    for file in parser.SVG_IMAGES:
+        handle_defined(file, folder / "images" / "SVG")
+        
+    for file in parser.AVI_VIDEO:
+        handle_defined(file, folder / "video" / "AVI")
+    for file in parser.MKV_VIDEO:
+        handle_defined(file, folder / "video" / "MKV")
+    for file in parser.MOV_VIDEO:
+        handle_defined(file, folder / "video" / "MOV")
+    for file in parser.MP4_VIDEO:
+        handle_defined(file, folder / "video" / "MP4")
+
+    for file in parser.AMR_AUDIO:
+        handle_defined(file, folder / "audio" / "AMR")
+    for file in parser.MP3_AUDIO:
+        handle_defined(file, folder / "audio" / "MP3")
+    for file in parser.OGG_AUDIO:
+        handle_defined(file, folder / "audio" / "OGG")
+    for file in parser.MP3_AUDIO:
+        handle_defined(file, folder / "audio" / "MP3")
+
+    for file in parser.DOC_DOCUMENTS:
+        handle_defined(file, folder / "documents" / "DOC")
+    for file in parser.DOCX_DOCUMENTS:
+        handle_defined(file, folder / "documents" / "DOCX")
+    for file in parser.PDF_DOCUMENTS:
+        handle_defined(file, folder / "documents" / "PDF")
+    for file in parser.PPTX_DOCUMENTS:
+        handle_defined(file, folder / "documents" / "PPTX")
+    for file in parser.TXT_DOCUMENTS:
+        handle_defined(file, folder / "documents" / "TXT")
+    for file in parser.XLS_DOCUMENTS:
+        handle_defined(file, folder / "documents" / "XLS")
+    for file in parser.XLSX_DOCUMENTS:
+        handle_defined(file, folder / "documents" / "XLSX")
+
+    for file in parser.GZ_ARCHIVES:
+        handle_archive(file, folder / "archives")
+    for file in parser.TAR_ARCHIVES:
+        handle_archive(file, folder / "archives")
+    for file in parser.ZIP_ARCHIVES:
+        handle_archive(file, folder / "archives")
+
+    for file in parser.MY_OTHER:
+        handle_other(file, folder / "MY_OTHER")
+
+    # Garbage collector
+    for folder in parser.FOLDERS[::-1]:
+        handle_folder(folder)
 
 
 
